57065d14bad2ad3909d48e5b947339c595954d90
FCHDev <acetige@gmail.com>
2022-09-12 09:54:55 +0200

    MaJ firebaseConfig.js

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const configInfos = {
-  apiKey: `${process.env.REACT_APP_CLE_API}`,
-  authDomain: `${process.env.GUITARIST_AUTH_DOMAIN}`,
-  projectId: `${process.env.PROJECT_ID}`,
-  storageBucket: `${process.env.STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
-  appId: `${process.env.APP_ID}`,
+  apiKey: process.env.REACT_APP_CLE_API,
+  authDomain: process.env.REACT_APP_GUITARIST_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
   databaseURL:
     "https://peoples-to-buy-default-rtdb.europe-west1.firebasedatabase.app/",
 };
